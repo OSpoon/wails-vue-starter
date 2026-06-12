25719909a549e800e549f0d72c631119cfc04b0d
OSpoon <zxin088@gmail.com>
2026-06-12 09:47:53 +0800

    chore(release): v0.0.2-beta.1

--- appconfig.go
+++ appconfig.go
@@ -4,7 +4,7 @@ const (
 	appName              = "wails-vue-starter"
 	appDisplayName       = "Wails Vue Starter"
 	appDescription       = "A Wails3 + Vue 3 + TypeScript starter template"
-	appVersion           = "0.0.1"
+	appVersion           = "0.0.2-beta.1"
 	appProductIdentifier = "com.ospoon.wails-vue-starter"
 	appRepository        = "OSpoon/wails-vue-starter"
 	appAuthor            = "OSpoon"
